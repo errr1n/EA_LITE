b8e2096c9e6cace548cf0a7a1760c3fe5ea8a44f
errr1n <eteply@telus.net>
2025-01-09 17:34:17 -0800

    small camera adjustment

--- EA_LITE/Assets/Scripts/Character/Player/PlayerCamera.cs
+++ EA_LITE/Assets/Scripts/Character/Player/PlayerCamera.cs
@@ -17,12 +17,17 @@ public class PlayerCamera : MonoBehaviour
     [SerializeField] float upAndDownRotationSpeed = 220;
     [SerializeField] float minimumPivot = -30; // THE LOWEST ANGLE WE CAN LOOK DOWN
     [SerializeField] float maximumPivot = 60; // THE HIGHEST ANGLE WE CAN LOOK UP
+    [SerializeField] float cameraCollisionRadius = 0.2f; // HOW CLOSE THE CAMERA CAN GET BEFORE COLLIDING WITH OBJECT
+    [SerializeField] LayerMask collideWithLayers;
 
     // JUST DISPLAYS CAMERA VALUES
     [Header("Camera Values")]
     private Vector3 cameraVelocity;
+    private Vector3 cameraObjectPosition; // USED FOR CAMERA COLLISIONS (MOVES THE CAMERA OBJECT TO THIS POSITION UPON COLLISION)
     [SerializeField] float leftAndRightLookAngle;
     [SerializeField] float upAndDownLookAngle;
+    private float cameraZPosition; // VALUES USED FOR CAMERA COLLISIONS
+    private float targetCameraZPosition; // VALUES USED FOR CAMERA COLLISIONS
 
     private void Awake()
     {
@@ -39,6 +44,8 @@ public class PlayerCamera : MonoBehaviour
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
+
+        cameraZPosition = cameraObject.transform.localPosition.z;
     }
 
     public void HandleAllCameraActions()
@@ -51,13 +58,14 @@ public class PlayerCamera : MonoBehaviour
             // ROTATE AROUND PLAYER
             HandleRotations();
             // COLLIDE WITH ENVIRONMENT
+            HandleCollisions();
         }
     }
 
     private void HandleFollowTarget()
     {
-        Vector3 targetCameraPosition = Vector3.SmoothDamp(transform.position, player.transform.position, ref cameraVelocity, cameraSmoothSpeed * Time.deltaTime);
-        transform.position = targetCameraPosition;
+        Vector3 targetCameraZPosition = Vector3.SmoothDamp(transform.position, player.transform.position, ref cameraVelocity, cameraSmoothSpeed * Time.deltaTime);
+        transform.position = targetCameraZPosition;
     }
 
     private void HandleRotations()
@@ -86,5 +94,33 @@ public class PlayerCamera : MonoBehaviour
         cameraRotation.x = upAndDownLookAngle;
         targetRotation = Quaternion.Euler(cameraRotation);
         cameraPivotTransform.localRotation = targetRotation;
+    }
+
+    private void HandleCollisions()
+    {
+        targetCameraZPosition = cameraZPosition;
+        RaycastHit hit;
+        // DIRECTION OF COLLISION CHECK
+        Vector3 direction = cameraObject.transform.position - cameraPivotTransform.position;
+        direction.Normalize();
+
+        // CHECK IF THERE IS AN OBJECT IN FRONT OF THE DESIRED DIRECTION OF CAMERA
+        if(Physics.SphereCast(cameraPivotTransform.position, cameraCollisionRadius, direction, out hit, Mathf.Abs(targetCameraZPosition), collideWithLayers))
+        {
+            // IF THERE IS, WE GET OUR DISTANCE FROM IT
+            float distanceFromHitObject = Vector3.Distance(cameraPivotTransform.position, hit.point);
+            // WE THEN EQUATE OUR TARGET Z POSITION TO THE FOLLOWING
+            targetCameraZPosition = -(distanceFromHitObject - cameraCollisionRadius);
+        }
+
+        // IF OUR TARGET POSITION IS LESS THAN OUR COLLISION RADIUS, WE SUBTRACT OUR COLLISION RADIUS (MAKING IT SNAP BACK)
+        if(Mathf.Abs(targetCameraZPosition) < cameraCollisionRadius)
+        {
+            targetCameraZPosition = -cameraCollisionRadius;
+        }
+
+        // WE THEN APPLY OUR FINAL POSITION USING A LERP OVER A TIME OF 0.2F
+        cameraObjectPosition.z = Mathf.Lerp(cameraObject.transform.localPosition.z, targetCameraZPosition, 0.2f);
+        cameraObject.transform.localPosition = cameraObjectPosition;
     }
 }
