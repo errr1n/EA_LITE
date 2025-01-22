4a2d9bac6c00b3b4ce6c49712df738489e4fbfaf
errr1n <eteply@telus.net>
2025-01-22 12:41:32 -0800

    state switch

--- EA_LITE/Assets/Scripts/Character/AI Character/IdleState.cs
+++ EA_LITE/Assets/Scripts/Character/AI Character/IdleState.cs
@@ -8,19 +8,19 @@ public class IdleState : AIState
 {
     public override AIState Tick(AICharacterManager aiCharacter)
     {
-        return base.Tick(aiCharacter);
+        // return base.Tick(aiCharacter);
 
-        // if(aiCharacter.characterCombatManager.currentTarget != null)
-        // {
-        //     // return the pursue target state
-        //     Debug.log("WE HAVE A TARGET");
-        // }
-        // else
-        // {
-        //     //return this state, to continually search for a target
-        //     Debug.log("WE HAVE NO TARGET");
-        // }
+        if(aiCharacter.characterCombatManager.currentTarget != null)
+        {
+            // return the pursue target state
+            Debug.Log("WE HAVE A TARGET");
+        }
+        else
+        {
+            //return this state, to continually search for a target
+            Debug.Log("WE HAVE NO TARGET");
+        }
 
-        // return this;
+        return this;
     }
 }
