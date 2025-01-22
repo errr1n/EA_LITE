c6db382df5d7432b4743a4529795d689bfd96064
errr1n <eteply@telus.net>
2025-01-22 12:49:18 -0800

    lock on target

---
+++ EA_LITE/Assets/Scripts/Character/AI Character/AICharacterCombatManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICharacterCombatManager : CharacterCombatManager
+{
+    // [Header("Detection")]
+    // [SerializeField] float detectionRadius = 15;
+    // [SerializeField] float minimumDetectionAngle = -35;
+    // [SerializeField] float maximumDetectionAngle = 35;
+
+    // public void FindATargetViaLineOfSight(AICharacterManager aiCharacter)
+    // {
+    //     if(currentTarget != null)
+    //     {
+    //         return;
+    //     }
+
+    //     Collider[] colliders = Physics.OverlapSphere(aiCharacter.transform.position, detectionRadius, WorldUtilityManager.instance.GetCharacterLayers());
+
+    //     for(int i = 0; i < colliders.Length; i++)
+    //     {
+    //         CharacterManager targetCharacter = colliders[i].transform.GetComponent<CharacterManager>();
+
+    //         if(targetCharacter == null)
+    //         {
+    //             continue;
+    //         }
+
+    //         if(targetCharacter == aiCharacter)
+    //         {
+    //             continue;
+    //         }
+                
+    //         if(targetCharacter.isDead)
+    //         {
+    //             continue;
+    //         }
+
+    //         // can i attack this character
+    //         if(WorldUtilityManager.instance.CanIDamageThsTarget(aiCharacter.characterGroup, targetCharacter.characterGroup))
+    //         {
+    //             // IF A POTENTIAL TARGET IS FOUND, IT HAS TO BE IN FRONT OF US
+    //             Vector3 targetDirection = targetCharacter.transform.position - aiCharacter.transform.position;
+    //             float viewableAngle = Vector3.Angle(targetDirection, aiCharacter.transform.forward);
+
+    //             if(viewableAngle > minimumDetectionAngle && viewableAngle < maximumDetectionAngle)
+    //             {
+    //                 // lastly, check for environmental blockage
+    //                 if(Physics.Linecast(aiCharacter.characterCombatManager.lockOnTransform.position, 
+    //                 targetCharacter.characterCombatManager.lockOnTransform.position, 
+    //                 WorldUtilityManager.instance.GetEnviroLayers()))
+    //                 {
+    //                     Debug.DrawLine(aiCharacter.characterCombatManager.lockOnTransform.position, targetCharacter.characterCombatManager.lockOnTransform.position);
+    //                     Debug.Log("BLOCKED");
+    //                 }
+    //                 else
+    //                 {
+    //                     aiCharacter.characterCombatManager.SetTarget(targetCharacter);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+}
