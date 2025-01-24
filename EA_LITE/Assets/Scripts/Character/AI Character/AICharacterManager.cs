e67cc0f77340edb54e98a8130809d5d56b7cf3ab
errr1n <eteply@telus.net>
2025-01-23 16:12:33 -0800

    nav mesh tracking

--- EA_LITE/Assets/Scripts/Character/AI Character/AICharacterManager.cs
+++ EA_LITE/Assets/Scripts/Character/AI Character/AICharacterManager.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AICharacterManager : CharacterManager
 {
-    public AICharacterCombatManager aiCharacterCombatManager;
+    [HideInInspector] public AICharacterCombatManager aiCharacterCombatManager;
+
+    [Header("Navmesh Agent")]
+    public NavMeshAgent navMeshAgent;
 
     [Header("Current State")]
     [SerializeField] AIState currentState;
+
+    [Header("States")]
+    [SerializeField] public IdleState idle;
+    [SerializeField] public PursueTargetState pursueTarget;
 
     protected override void Awake()
     {
         base.Awake();
 
         aiCharacterCombatManager = GetComponent<AICharacterCombatManager>();
+
+        navMeshAgent = GetComponentInChildren<NavMeshAgent>();
+
+        // use a copy of the scriptable object. so the originals are not modified
+        idle = Instantiate(idle);
+        pursueTarget = Instantiate(pursueTarget);
+
+        currentState = idle;
     }
 
     protected override void FixedUpdate()
--- EA_LITE/Assets/Scripts/Character/AI Character/PursueTargetState.cs
+++ EA_LITE/Assets/Scripts/Character/AI Character/PursueTargetState.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[CreateAssetMenu(menuName = "A.I/States/Pursue Target")]
 public class PursueTargetState : AIState
 {
     public override AIState Tick(AICharacterManager aiCharacter)
     {
-        return base.Tick(aiCharacter);
+        // Debug.Log("PURSUE TARGET");
+        // return base.Tick(aiCharacter);
 
         // CHECK IF WE'RE PERFORMING AN ACTION (DO NOT MOVE)
+        if(aiCharacter.isPerformingAction)
+        {
+            return this;
+        }
 
         // CHECK IF TARGET IS NULL, IF WE DO NOT HAVE A TARGET RETURN TO IDLE
+        if(aiCharacter.aiCharacterCombatManager.currentTarget == null)
+        {
+            return SwitchState(aiCharacter, aiCharacter.idle);
+        }
 
         // MAKE SURE NAV MESH AGENT IS ACTIVE, IF NOT ENABLE
+        if(!aiCharacter.navMeshAgent.enabled)
+        {
+            aiCharacter.navMeshAgent.enabled = true;
+        }
 
         // IF WITHIN COMBAT RANGE, SWITCH TO COMBAT STATE
 
         // IF THE TARGET IS NOT REACHABLE AND FAR AWAY, RETURN HOME
 
         // PURSUE THE TARGET
+        NavMeshPath path = new NavMeshPath();
+        aiCharacter.navMeshAgent.CalculatePath(aiCharacter.aiCharacterCombatManager.currentTarget.transform.position, path);
+        aiCharacter.navMeshAgent.SetPath(path);
+
+        return this;
     }
 }
