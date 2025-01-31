58e5ee308711494076879dcbdb01c42c7f82b74d
errr1n <eteply@telus.net>
2025-01-30 16:21:37 -0800

    bringing in boss

---
+++ EA_LITE/Assets/AIBossCharacterManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBossCharacterManager : MonoBehaviour
+{
+    // GIVE THIS AI A UNIQUE ID
+    public int bossID = 0;
+    // IF THE SAVE FILE DOES NOT CONTAIN A BOSS MONSTER OF THIS ID ADD IT
+    // IF IT IS PRESENT, CHECK IF BOSS HAS BEEN DEFEATED
+    // IF THE BOSS WAS DEFEATED, DIABLE THE GAMEOBJECT
+    // IF THE BOSS HAS NOT BEEN DEFEATED, ALLOW THIS OBJECT TO CONTINUE TO BE ACTIVE
+}
