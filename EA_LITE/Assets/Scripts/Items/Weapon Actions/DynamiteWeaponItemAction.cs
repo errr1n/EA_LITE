fe7b09acb72edfbe1db2e478327d4d77a29f153e
errr1n <eteply@telus.net>
2025-02-17 15:35:46 -0800

    swap to dynamite weapon

---
+++ EA_LITE/Assets/Scripts/Items/Weapon Actions/DynamiteWeaponItemAction.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Character Actions/Weapon Actions/Dynamite Action")]
+
+public class DynamiteWeaponItemAction : WeaponItemAction
+{
+    // ANIMATION STRING
+    // [SerializeField] string Place_Dynamite = "Place_Dynamite";
+
+    public override void AttemptToPerformAction(PlayerManager playerPerformingAction, WeaponItem weaponPerformingAction)
+    {
+        base.AttemptToPerformAction(playerPerformingAction, weaponPerformingAction);
+
+        // check for stops 
+        // stamina
+        //if jumping
+        //if dodging
+        if(!playerPerformingAction.isPerformingAction)
+        {
+            PerformDynamiteAttack(playerPerformingAction, weaponPerformingAction);
+        }
+        // PerformLightAttack(playerPerformingAction, weaponPerformingAction);
+    }
+
+    private void PerformDynamiteAttack(PlayerManager playerPerformingAction, WeaponItem weaponPerformingAction)
+    {
+        if(playerPerformingAction.isUsingRightHand)
+        {
+            // PLAY ANIMATION
+            // playerPerformingAction.playerAnimatorManager.PlayTargetAttackActionAnimation(AttackType.LightAttack, Light_Attack, true);
+            Debug.Log("PLACE DYNAMITE");
+        }
+    }
+}
