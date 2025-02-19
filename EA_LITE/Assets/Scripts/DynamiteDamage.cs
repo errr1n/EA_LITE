509e7356af0df1a121d1afd5f5e499a4d225faa7
errr1n <eteply@telus.net>
2025-02-18 16:04:47 -0800

    destroy particle from scene after 2.5 seconds passes

--- EA_LITE/Assets/Scripts/DynamiteDamage.cs
+++ EA_LITE/Assets/Scripts/DynamiteDamage.cs
@@ -4,85 +4,16 @@ using UnityEngine;
 
 public class DynamiteDamage : DamageCollider
 {
+    // public CharacterManager setDamageTarget;
     [SerializeField] public int dynamiteDamage = 50;
 
-    public CharacterManager setDamageTarget;
-
     [SerializeField] GameObject explosionParticle;
-    GameObject pParticle;
+    GameObject eParticle;
 
-    // public DynamiteWeaponItemAction dynamiteItem;
-
-    // [SerializeField] GameObject poisonParticle;
-    // GameObject pParticle;
-
-    // Vector3 bulletPosition;
-    // Vector3 dynamitePosition;
-
-  
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        // DelayDestroy();
-        // StartCoroutine(Delay());
-
-        // dynamiteItem = GetComponent<DynamiteWeaponItemAction>();
-
-        // dynamiteDamage.CheckIfDamageable();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        // Invoke(nameof(DestroyShot), 2f);
         StartCoroutine(CheckIfDamageable());
-        // CheckIfDamageable();
     }
-
-    // private void OnTriggerEnter(Collider other)
-    // {
-      
-    //     float timer;
-    //     if(other.GetComponent<BulletTarget>() != null)
-    //     {
-    //         //hit target (can play particles from here)
-    //         //play particle effect (effet rotated 90 degrees))
-    //         pParticle = Instantiate(poisonParticle, transform.position, Quaternion.Euler(90,0,0));
-    //         //destory particle after 3 seconds
-    //         Destroy(pParticle, 3);
-   
-    //         // calls check if burnable method
-    //         CheckIfBurnable();
-    //     } 
-    //     //destroy bullet
-    //     Destroy(gameObject);
-      
-    // }
-
-    // private void OnCollisionEnter(Collision collision)
-    // {
-    //     // float timer;
-    //     if(collision.collider.tag == "isGround")
-    //     {
-    //         //hit target (can play particles from here)
-    //         //play particle effect (effet rotated 90 degrees))
-    //         // pParticle = Instantiate(poisonParticle, transform.position, Quaternion.Euler(90,0,0));
-    //         //destory particle after 3 seconds
-    //         // Destroy(pParticle, 3);
-   
-    //         // calls check if burnable method
-    //         CheckIfDamageable();
-           
-    //         // bulletPosition = transform.position;
-    //         // Debug.Log(transform.position);
-    //         // Debug.Log("bullet position " + bulletPosition);
-    //     } 
-    //     //destroy bullet
-    //     Destroy(gameObject);
-
-        
-    // }
 
     //check if an AOETarget is within the sphere
     public IEnumerator CheckIfDamageable()
@@ -93,13 +24,13 @@ public class DynamiteDamage : DamageCollider
         // dynamitePosition = dynamiteItem.instantiatedGameObject.transform.position;
         //creates a list of colliders and creates a 4m overlap sphere (sphere collider)
         Collider[] colliders = Physics.OverlapSphere(transform.position, 4f);
-        foreach(Collider c in colliders)
+        foreach(Collider collider in colliders)
         {
             //if their is an AOETarget script attached to object
-            if(c.GetComponent<AOETarget>())
+            if(collider.GetComponent<AOETarget>())
             {
-                CharacterManager damageTarget = c.GetComponent<CharacterManager>();
-                setDamageTarget = damageTarget;
+                CharacterManager damageTarget = collider.GetComponent<CharacterManager>();
+                // setDamageTarget = damageTarget;
 
                 if(damageTarget != null)
                 {
@@ -122,18 +53,19 @@ public class DynamiteDamage : DamageCollider
                     //PRINT WHAT COLLIDER IS HIT
                     // Debug.Log(damageTarget);
 
-                    // c.GetComponent<AOETarget>().ApplyDamage(dynamiteDamage);
+                    // collider.GetComponent<AOETarget>().ApplyDamage(dynamiteDamage);
                 }
 
                 //apply the burn method to the object with the AOETarget script
-                // c.GetComponent<AOETarget>().ApplyDamage(dynamiteDamage);
+                // collider.GetComponent<AOETarget>().ApplyDamage(dynamiteDamage);
 
                 // character.ProcessCharacterDamage();
                 // DamageTarget(setDamageTarget);
                 // Debug.Log("0");
             }
         }
-        pParticle = Instantiate(explosionParticle, transform.position, Quaternion.Euler(0,0,0));
+        eParticle = Instantiate(explosionParticle, transform.position, Quaternion.Euler(0,0,0));
+        Destroy(eParticle, 2.5f);
         Destroy(gameObject);
     }
 
